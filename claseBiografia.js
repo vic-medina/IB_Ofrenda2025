66128b51c566fed19d4a43121d58d310cac925df
vic-medina <79273730+vic-medina@users.noreply.github.com>
2025-10-30 11:19:58 -0600

    Soporte
    Ajustes Bio

--- claseBiografia.js
+++ claseBiografia.js
@@ -3,7 +3,7 @@ const texturas = {
 };
 
 class Biografia {
-  constructor(id, txFrontal3, txFrontal4, texturaTrasera, scale = '2.5 2.5 2.5', position = '0 2.5 0') {
+  constructor(id, txFrontal3, txFrontal4, texturaTrasera, scale = '1 1 1', position = '0 1.5 0') {
     this.id = id;
     this.texturasFrontales = [texturas.comunes[0], texturas.comunes[1], txFrontal3, txFrontal4, texturas.comunes[2]];
     this.texturaTrasera = texturaTrasera;
